bc0c14e9a2e30734e1c10c75508dc7360cf2a5d2
Marek Rusinowski <marekrusinowski@gmail.com>
2024-09-21 17:55:20 +0200

    Add extraction of user account tables

--- scripts/export_prod_data_source.sql
+++ scripts/export_prod_data_source.sql
@@ -5,6 +5,8 @@ ATTACH 'dbname=teiserver_prod' AS teiserver (TYPE POSTGRES, READ_ONLY);
 COPY teiserver.public.teiserver_battle_matches TO 'data_export/teiserver_battle_matches.parquet' (FORMAT 'parquet', CODEC 'zstd', COMPRESSION_LEVEL 9);
 COPY teiserver.public.teiserver_battle_match_memberships TO 'data_export/teiserver_battle_match_memberships.parquet' (FORMAT 'parquet', CODEC 'zstd', COMPRESSION_LEVEL 9);
 COPY teiserver.public.teiserver_game_rating_logs TO 'data_export/teiserver_game_rating_logs.parquet' (FORMAT 'parquet', CODEC 'zstd', COMPRESSION_LEVEL 9);
+COPY (SELECT * EXCLUDE (password) FROM teiserver.public.account_users) TO 'data_export/teiserver_users.parquet' (FORMAT 'parquet', CODEC 'zstd', COMPRESSION_LEVEL 9);
+COPY teiserver.public.teiserver_account_user_stats TO 'data_export/teiserver_user_stats.parquet' (FORMAT 'parquet', CODEC 'zstd', COMPRESSION_LEVEL 9);
 
 ATTACH 'dbname=bar' AS replay (TYPE POSTGRES, READ_ONLY);
 
