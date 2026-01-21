3094c6b3528cd73a546118f815a2ba67ba6295f7
Zain Mustafa <108363829+ZainMGit@users.noreply.github.com>
2026-01-21 07:49:01 -0800

    Update player_skill_snapshot.sql
    lint check fix, ran sqlfluff

--- models/intermediate/player_skill_snapshot.sql
+++ models/intermediate/player_skill_snapshot.sql
@@ -5,6 +5,7 @@ WITH
     SELECT
       match_id,
       start_time,
+      game_type,
       lower(game_type) AS game_type_l
     FROM {{ ref("matches") }}
     WHERE is_ranked = true
@@ -26,40 +27,41 @@ WITH
   ),
   joined AS (
     SELECT
+      m.start_time,
       mp.user_id,
       p.name,
       p.countryCode,
-      m.start_time,
       CASE
         WHEN m.game_type_l LIKE '%duel%' THEN 'duel'
         WHEN m.game_type_l LIKE '%ffa%' THEN 'ffa'
         WHEN m.game_type_l LIKE '%large%' THEN 'large'
         WHEN m.game_type_l LIKE '%small%' THEN 'small'
-        ELSE NULL
       END AS game_type,
       mp.new_skill,
       mp.new_uncertainty
-    FROM match_players mp
-    JOIN matches m USING (match_id)
-    JOIN players p USING (user_id)
+    FROM match_players AS mp
+    JOIN matches AS m
+      ON mp.match_id = m.match_id
+    JOIN players AS p
+      ON mp.user_id = p.user_id
   )
 SELECT
   user_id AS id,
   name,
-  arg_max(new_skill, start_time) FILTER (WHERE game_type = 'duel')  AS duelSkill,
+  countryCode,
+  arg_max(new_skill, start_time) FILTER (WHERE game_type = 'duel') AS duelSkill,
   arg_max(new_uncertainty, start_time) FILTER (WHERE game_type = 'duel') AS duelSkillUn,
-  arg_max(new_skill, start_time) FILTER (WHERE game_type = 'ffa')   AS ffaSkill,
-  arg_max(new_uncertainty, start_time) FILTER (WHERE game_type = 'ffa')  AS ffaSkillUn,
+  arg_max(new_skill, start_time) FILTER (WHERE game_type = 'ffa') AS ffaSkill,
+  arg_max(new_uncertainty, start_time) FILTER (WHERE game_type = 'ffa') AS ffaSkillUn,
   arg_max(new_skill, start_time) FILTER (WHERE game_type = 'large') AS teamSkill,
   arg_max(new_uncertainty, start_time) FILTER (WHERE game_type = 'large') AS teamSkillUn,
-  arg_max(start_time, start_time) FILTER (WHERE game_type = 'duel')  AS lastDuel,
-  arg_max(start_time, start_time) FILTER (WHERE game_type = 'ffa')   AS lastFFA,
+  arg_max(start_time, start_time) FILTER (WHERE game_type = 'duel') AS lastDuel,
+  arg_max(start_time, start_time) FILTER (WHERE game_type = 'ffa') AS lastFFA,
   arg_max(start_time, start_time) FILTER (WHERE game_type = 'large') AS lastTeam,
-  countryCode,
   arg_max(start_time, start_time) FILTER (WHERE game_type = 'small') AS lastSmallTeam,
-  arg_max(new_skill, start_time) FILTER (WHERE game_type = 'small')  AS smallTeamSkill,
+  arg_max(new_skill, start_time) FILTER (WHERE game_type = 'small') AS smallTeamSkill,
   arg_max(new_uncertainty, start_time) FILTER (WHERE game_type = 'small') AS smallTeamSkillUn
 FROM joined
-WHERE game_type IS NOT NULL
+WHERE game_type IS NOT null
 GROUP BY user_id, name, countryCode
 ORDER BY user_id
