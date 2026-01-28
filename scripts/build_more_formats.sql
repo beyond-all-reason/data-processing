5f47f807d031538169ce9ce081fb1f2e17abf40f
Zain Mustafa <108363829+ZainMGit@users.noreply.github.com>
2026-01-28 14:47:01 -0800

    Update build_more_formats.sql
    redundant

--- scripts/build_more_formats.sql
+++ scripts/build_more_formats.sql
@@ -3,4 +3,4 @@
 COPY 'data_export/matches.parquet' TO 'data_export/matches.csv.gz';
 COPY 'data_export/match_players.parquet' TO 'data_export/match_players.csv.gz';
 COPY 'data_export/players.parquet' TO 'data_export/players.csv.gz';
-COPY 'data_export/player_skill_snapshot.parquet' TO 'data_export/player_skill_snapshot.csv' (HEADER);
+COPY 'data_export/player_skill_snapshot.parquet' TO 'data_export/player_skill_snapshot.csv';
