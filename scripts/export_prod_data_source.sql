89444117d8add3c80b5449e45cbe72aacac55457
Bruno Da Silva <Bruno-DaSilva@users.noreply.github.com>
2026-04-15 19:44:39 -0400

    feat: add benchmark events as sources
    # Purpose
    See #7. The goal is to take the "complex events" in teiserver that match system:benchmark and make them available as data for dashboarding.
    
    ## Privacy
    I'm not sure our stance on this but I've defaulted to **excluding** user_id from the export (it's not needed at all) so people can't identify the hardware of specific users.
    
    An example payload in the `value` column is this: `{"Sim": {"mean": 13.961731, "count": 1800, "total": 25131.1152, "spread": 1.27117431, "percentiles": {"0": 9.9630003, "1": 11.0419998, "2": 11.2279997, "5": 11.6110001, "10": 12.0139999, "20": 12.5120001, "35": 13.2309999, "50": 13.8470001, "65": 14.46, "80": 15.1219997, "90": 15.9379997, "95": 16.7099991, "98": 17.7919998, "99": 18.5750008, "100": 43.8349991}}, "cpu": "AMD Ryzen 9 7950X3D 16-Core Processor          ; 64632MB RAM, 73336MB pagefile", "gpu": "NVIDIA GeForce RTX 5070 Ti/PCIe/SSE2", "Draw": {"mean": 9.84391785, "count": 2388, "total": 23507.2754, "spread": 3.77889252, "percentiles": {"0": 1.70700002, "1": 1.74100006, "2": 1.75399995, "5": 1.85000002, "10": 2.12700009, "20": 3.73200011, "35": 8.6960001, "50": 10.9720001, "65": 12.3769999, "80": 13.1339998, "90": 14.1190004, "95": 16.1040001, "98": 18.6919994, "99": 20.1100006, "100": 158.367004}}, "Update": {"mean": 1.88539255, "count": 2388, "total": 4502.31738, "spread": 0.9580617, "percentiles": {"0": 0.214, "1": 0.227, "2": 0.23100001, "5": 0.245, "10": 0.29499999, "20": 0.588, "35": 1.079, "50": 2.43099999, "65": 2.54900002, "80": 2.67600012, "90": 2.83500004, "95": 2.96399999, "98": 3.16599989, "99": 3.61100006, "100": 8.98400021}}, "display": "3840x1600", "mapName": "Starwatcher 1.0", "gameName": "Beyond All Reason test-29918-105c4e6", "engineVersion": "2025.06.19", "benchmarkcommand": "luarules fightertest corak armpw 650 10 2040"}` which, as you can tell, has no PII. So it should be safe to export here anonymously.
    
    ## Next steps
    A follow up PR will be created to make intermediate/marts but I am holding off on that until a sample of data is exported for the first time (so I can test it locally).

--- scripts/export_prod_data_source.sql
+++ scripts/export_prod_data_source.sql
@@ -8,6 +8,22 @@ COPY teiserver.public.teiserver_game_rating_logs TO 'data_export/teiserver_game_
 COPY (SELECT * EXCLUDE (password) FROM teiserver.public.account_users) TO 'data_export/teiserver_users.parquet' (FORMAT 'parquet', CODEC 'zstd', COMPRESSION_LEVEL 9);
 COPY teiserver.public.teiserver_account_user_stats TO 'data_export/teiserver_user_stats.parquet' (FORMAT 'parquet', CODEC 'zstd', COMPRESSION_LEVEL 9);
 
+COPY (
+  SELECT e.id, e.timestamp, e.value, false AS is_anon
+  FROM teiserver.public.analytics_complex_client_events AS e
+  INNER JOIN teiserver.public.analytics_complex_client_event_types AS t
+    ON e.event_type_id = t.id
+  WHERE t.name = 'system:benchmark'
+) TO 'data_export/teiserver_benchmark_events.parquet' (FORMAT 'parquet', CODEC 'zstd', COMPRESSION_LEVEL 9);
+
+COPY (
+  SELECT e.id, e.timestamp, e.value, true AS is_anon
+  FROM teiserver.public.analytics_complex_anon_events AS e
+  INNER JOIN teiserver.public.analytics_complex_anon_event_types AS t
+    ON e.event_type_id = t.id
+  WHERE t.name = 'system:benchmark'
+) TO 'data_export/teiserver_benchmark_anon_events.parquet' (FORMAT 'parquet', CODEC 'zstd', COMPRESSION_LEVEL 9);
+
 ATTACH 'dbname=bar' AS replay (TYPE POSTGRES, READ_ONLY);
 
 COPY replay.public.Demos TO 'data_export/replay_demos.parquet' (FORMAT 'parquet', CODEC 'zstd', COMPRESSION_LEVEL 9);
